dbf927a31b44b52f7678cd5e55bd1b039e06b0c9
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-05-16 16:15:06 +0500

    Create Database and Table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
+
+\c vet_clinic;
 
 CREATE TABLE animals (
-    name varchar(100)
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+date_of_birth date,
+escape_attempts integer,
+neutered boolean,
+weight_kg decimal
 );
