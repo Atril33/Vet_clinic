fd1ff8682969688aebe35d37a946d1c56da08a8f
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-05-18 10:52:03 +0500

    Add More Filter Conditions in queries.sql File';

--- queries.sql
+++ queries.sql
@@ -8,3 +8,33 @@ SELECT name, escape_attempts  FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = 'true';
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.3 AND weight_kg <= 17.3;
+
+/* Day 2. */
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+UPDATE animals SET species = 'digimon' WHERE name like '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+END;
+
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
+END;
+
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth >= '2022-01-01';
+SAVEPOINT save_point;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT save_point;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(animals) FROM animals WHERE escape_attempts <= 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT MAX(escape_attempts), neutered FROM animals GROUP BY neutered;
+SELECT MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth >= '1990-12-31' AND date_of_birth <= '2000-01-01' GROUP BY species;
